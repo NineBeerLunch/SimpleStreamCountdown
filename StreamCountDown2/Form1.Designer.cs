6c0471fdbab5f6aa02b34ad017106a9d0fd3dd60
NineBeerLunch <57721415+NineBeerLunch@users.noreply.github.com>
2019-11-13 10:16:46 -0600

    Uploaded the first iteration that works

---
+++ StreamCountDown2/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace StreamCountDown2
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            this.timCountDown = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtMinute = new System.Windows.Forms.TextBox();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timCountDown
+            // 
+            this.timCountDown.Interval = 1000;
+            this.timCountDown.Tick += new System.EventHandler(this.timCountDown_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 57);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(146, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start Count Down";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(12, 57);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(146, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop Count Down";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = ":";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 96);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(146, 20);
+            this.txtMessage.TabIndex = 5;
+            this.txtMessage.Text = "Let\'s Go !!!!!1!";
+            // 
+            // txtMinute
+            // 
+            this.txtMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMinute.Location = new System.Drawing.Point(25, 13);
+            this.txtMinute.Name = "txtMinute";
+            this.txtMinute.Size = new System.Drawing.Size(43, 44);
+            this.txtMinute.TabIndex = 6;
+            this.txtMinute.Text = "10";
+            this.txtMinute.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSecond.Location = new System.Drawing.Point(96, 12);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(42, 44);
+            this.txtSecond.TabIndex = 7;
+            this.txtSecond.Text = "00";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(168, 135);
+            this.Controls.Add(this.txtSecond);
+            this.Controls.Add(this.txtMinute);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Stream Count Down";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Timer timCountDown;
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.Button btnStop;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtMessage;
+		private System.Windows.Forms.TextBox txtMinute;
+		private System.Windows.Forms.TextBox txtSecond;
+	}
+}
+
---
+++ StreamCountDown2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace StreamCountDown2
+{
+	public partial class frmMain : Form
+	{
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		private void btnStop_Click(object sender, EventArgs e)
+		{
+			btnStop.Visible = false;
+			btnStart.Visible = true;
+			timCountDown.Enabled = false;
+		}
+
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			btnStart.Visible = false;
+			btnStop.Visible = true;
+			timCountDown.Enabled = true;
+		}
+
+		private void timCountDown_Tick(object sender, EventArgs e)
+		{
+			int min = Convert.ToInt16(txtMinute.Text);
+			int sec = Convert.ToInt16(txtSecond.Text);
+
+			if (sec == 0)
+			{
+				sec = 59;
+				min = min - 1;
+			}
+			else
+			{
+				sec = sec - 1;
+			}
+
+			updateLabel(min, sec);
+			if (min == 0 && sec == 0)
+			{
+				updateFile(txtMessage.Text);
+				timCountDown.Enabled = false;
+				btnStart.Visible = true;
+				btnStop.Visible = false;
+			}
+		   else
+			{
+				if (min == 0)
+				{
+					updateFile(sec.ToString() + " Seconds");
+				}
+				else
+				{
+					updateFile(min, sec);
+				}
+			}
+
+		}
+
+		private void updateLabel(int min, int sec)
+		{
+			txtMinute.Text = min.ToString();
+			txtSecond.Text = sec.ToString("00");
+		}
+
+
+
+		private void updateFile(int min, int sec)
+		{
+			var directory = System.IO.Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+			StreamWriter sw = new StreamWriter(directory + "\\countdown.txt");
+
+			sw.WriteLine(min.ToString() + ":" + sec.ToString("00"));
+			sw.Close();
+			sw.Dispose();
+		}
+
+
+		private void updateFile(string message)
+		{
+			var directory = System.IO.Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+			StreamWriter sw = new StreamWriter(directory + "\\countdown.txt");
+
+			sw.WriteLine(message);
+			sw.Close();
+			sw.Dispose();
+		}
+
+	}
+}
